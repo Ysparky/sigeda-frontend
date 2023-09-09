4da2c77a471753010c55d88a9a68a97e37a7fac6
jcaldas <jos.caldas.27@gmail.com>
2023-09-09 02:47:45 -0500

    [feat] login view

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="input-form">
+    <label class="input-label"> {{ label }} :</label>
+    <v-text-field
+      v-model="model"
+      class="input-text"
+      :type="type"
+      :placeholder="placeholder"
+      dense
+      flat
+      outlined
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    label: { type: String, default: "" },
+    placeholder: { type: String, default: "" },
+    type: { type: String, default: "text" },
+    value: { type: String, default: "" },
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+  data: () => ({}),
+};
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="d-flex fill-height justify-center align-center bg-login">
+    <v-card
+      class="elevation-3 d-flex flex-column"
+      style="background: #f8f7fb; padding: 4rem 3rem"
+    >
+      <v-img
+        style="margin: auto; margin-bottom: 3rem"
+        max-height="200"
+        max-width="200"
+        src="../assets/images/logo-empresa.png"
+      ></v-img>
+      <base-input label="Código" placeholder="Código de alumno" />
+      <base-input label="Contraseña" placeholder="*************" />
+      <v-btn
+        class="my-3 pa-5"
+        depressed
+        flat
+        rounded
+        style="color: #ffffff; background: #2f80ed; margin: auto"
+      >
+        Ingresar
+      </v-btn>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import BaseInput from "@/components/BaseInput.vue";
+export default {
+  components: {
+    BaseInput,
+  },
+  name: "LoginView",
+};
+</script>
+
+<style lang="scss" scoped>
+.bg-login {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("../assets/images/bg-fpe.png");
+  background-position: 50% 0;
+  background-size: cover;
+}
+</style>
